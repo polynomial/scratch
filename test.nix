8558a866ea445072fdb6d10ade7bf8e0e90e868c
Ben Smith <ben.smith@lookout.com>
2015-08-11 10:43:41 -0700

    ec2 keys test

--- test.nix
+++ test.nix
@@ -31,9 +31,9 @@ in rec {
       buildCommand = ''
         export NIX_REMOTE=daemon
         set -x
-        curl --silent http://169.254.169.254/latest/meta-data
-        curl --silent http://169.254.169.254/latest/meta-data/iam/security-credentials
-        curl --silent http://169.254.169.254/latest/meta-data/iam/security-credentials/ci-deploy
+        curl http://169.254.169.254/latest/meta-data
+        curl http://169.254.169.254/latest/meta-data/iam/security-credentials
+        curl http://169.254.169.254/latest/meta-data/iam/security-credentials/ci-deploy
         access_key_id=$(curl --silent http://169.254.169.254/latest/meta-data/iam/security-credentials/ci-deploy | jq '.AccessKeyId' |sed 's/"//g')
         secret_access_key=$(curl --silent http://169.254.169.254/latest/meta-data/iam/security-credentials/ci-deploy | jq '.SecretAccessKey' |sed 's/"//g')
         if [ -n "$access_key_id" -a -n "$secret_access_key" -a ! -f $HOME/.ec2-keys ]; then
