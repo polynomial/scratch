f67b2e3aa2057cc531c5865e7afdf0b79fd85140
Ben Smith <ben.smith@lookout.com>
2015-08-11 13:12:49 -0700

    ec2 keys test

--- test.nix
+++ test.nix
@@ -29,16 +29,16 @@ in rec {
       ];
       name = "keymaster-release";
       buildCommand = ''
-        declare -r NIX_REMOTE=daemon
-        declare -r Z_DEPLOYMENT_ENV_TYPE="dev"
-        declare -r Z_DEPLOYMENT_TARGET="ec2"
-        declare -r Z_DEPLOYMENT_PROFILE="singlenode"
+        declare -r -x NIX_REMOTE=daemon
+        declare -r -x Z_DEPLOYMENT_ENV_TYPE="dev"
+        declare -r -x Z_DEPLOYMENT_TARGET="ec2"
+        declare -r -x Z_DEPLOYMENT_PROFILE="singlenode"
         set -x
         curl http://169.254.169.254/latest/meta-data
         curl http://169.254.169.254/latest/meta-data/iam/security-credentials
         curl http://169.254.169.254/latest/meta-data/iam/security-credentials/ci-deploy
-        declare -r AWS_ACCESS_KEY_ID=$(curl --silent http://169.254.169.254/latest/meta-data/iam/security-credentials/ci-deploy | jq '.AccessKeyId' |sed 's/"//g')
-        declare -r AWS_SECRET_ACCESS_KEY=$(curl --silent http://169.254.169.254/latest/meta-data/iam/security-credentials/ci-deploy | jq '.SecretAccessKey' |sed 's/"//g')
+        declare -r -x AWS_ACCESS_KEY_ID=$(curl --silent http://169.254.169.254/latest/meta-data/iam/security-credentials/ci-deploy | jq '.AccessKeyId' |sed 's/"//g')
+        declare -r -x AWS_SECRET_ACCESS_KEY=$(curl --silent http://169.254.169.254/latest/meta-data/iam/security-credentials/ci-deploy | jq '.SecretAccessKey' |sed 's/"//g')
         mkdir -p $out
         cd ${keymaster}
         ./z/bin/nixops-provision | tee $out/nixops-provision.log
