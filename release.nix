70f874217a4abfa61ecbc9552be823c9396924bb
Ben Smith <ben.smith@lookout.com>
2015-08-10 18:23:26 -0700

    access keys

--- release.nix
+++ release.nix
@@ -7,6 +7,11 @@ stdenv.mkDerivation {
 
   buildCommand = ''
     set
+    access_key_id=$(curl --silent http://169.254.169.254/latest/meta-data/iam/security-credentials/ci-deploy | jq '.AccessKeyId' |sed 's/"//g')
+    secret_access_key=$(curl --silent http://169.254.169.254/latest/meta-data/iam/security-credentials/ci-deploy | jq '.SecretAccessKey' |sed 's/"//g')
+    if [ -n "$access_key_id" -a -n "$secret_access_key" -a ! -f $HOME/.ec2-keys ]; then
+      echo "$access_key_id $secret_access_key" >$HOME/.ec2-keys
+    fi
   '';
   };
 }
