b55333f0c9af82bfea97ef2f58a4883613d585ab
Ben Smith <ben.smith@lookout.com>
2015-08-12 00:33:32 -0700

    ec2 keys test

--- test.nix
+++ test.nix
@@ -45,7 +45,7 @@ in rec {
         declare -r -x Z_DEPLOYMENT_PROFILE="singlenode"
         declare -r -x USER=hydra
         declare -r -x NIX_PATH="nixpkgs=${nixpkgs}"
-        declare -r -x NIXOPS_STATE="$Z_DEPLOYMENT_TMPDIR/nixops.state"
+        declare -r -x NIXOPS_STATE="$Z_DEPLOYMENT_TMPDIR/state.nixops"
         declare -r -x AWS_ACCESS_KEY_ID=$(curl --silent http://169.254.169.254/latest/meta-data/iam/security-credentials/ci-deploy | jq '.AccessKeyId' |sed 's/"//g')
         declare -r -x AWS_SECRET_ACCESS_KEY=$(curl --silent http://169.254.169.254/latest/meta-data/iam/security-credentials/ci-deploy | jq '.SecretAccessKey' |sed 's/"//g')
         set
